LuisMitaHL/LooperWrt

commit dbb95c2cc7e005fa1bcf008afac9a9af86ae436e

ramips: set default BLOCKSIZE to 64k for nor flash devices
In kernel 5.15, the default erase sector size of the nor flash has
been switched from 4k to 64k. This may cause the configuration not
be preserved across upgrades. To avoid this issue, change the default
BLOCKSIZE to 64k.

Signed-off-by: Shiji Yang <yangshiji66@qq.com>

target/linux/ramips/image/rt288x.mk
@@ -12,11 +12,10 @@ define Build/gemtek-header
 endef
 
 define Device/airlink101_ar670w
-  BLOCKSIZE := 64k
   DEVICE_VENDOR := Airlink
   DEVICE_MODEL := AR670W
   IMAGE_SIZE := 3840k
-  KERNEL := $(KERNEL_DTB) | pad-to $$(BLOCKSIZE)
+  KERNEL := $(KERNEL_DTB) | pad-to $$$$(BLOCKSIZE)
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | \
 	wrg-header wrgn16a_airlink_ar670w
@@ -38,7 +37,6 @@ endef
 TARGET_DEVICES += airlink101_ar725w
 
 define Device/asus_rt-n15
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := RT-N15
@@ -60,7 +58,6 @@ endef
 TARGET_DEVICES += belkin_f5d8235-v1
 
 define Device/buffalo_wli-tx4-ag300n
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Buffalo
   DEVICE_MODEL := WLI-TX4-AG300N
@@ -71,7 +68,6 @@ endef
 TARGET_DEVICES += buffalo_wli-tx4-ag300n
 
 define Device/buffalo_wzr-agl300nh
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Buffalo
   DEVICE_MODEL := WZR-AGL300NH
@@ -82,7 +78,6 @@ endef
 TARGET_DEVICES += buffalo_wzr-agl300nh
 
 define Device/dlink_dap-1522-a1
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3712k
   DEVICE_VENDOR := D-Link
   DEVICE_MODEL := DAP-1522
@@ -98,7 +93,6 @@ endef
 TARGET_DEVICES += dlink_dap-1522-a1
 
 define Device/ralink_v11st-fe
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Ralink
   DEVICE_MODEL := V11ST-FE

target/linux/ramips/image/Makefile
@@ -172,6 +172,7 @@ endef
 
 define Device/Default
   PROFILES = Default
+  BLOCKSIZE := 64k
   KERNEL := $(KERNEL_DTB) | uImage lzma
   KERNEL_LOADADDR := $(loadaddr-y)
   LZMA_TEXT_START := 0x81800000
@@ -185,7 +186,6 @@ define Device/Default
 endef
 
 define Device/netgear_sercomm_nor
-  BLOCKSIZE := 64k
   DEVICE_VENDOR := NETGEAR
   IMAGES += factory.img
   IMAGE/default := append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | \
@@ -197,7 +197,6 @@ define Device/netgear_sercomm_nor
 endef
 
 define Device/seama
-  BLOCKSIZE := 64k
   SEAMA_MTDBLOCK := 2
   IMAGES += factory.bin
 

target/linux/ramips/image/mt7620.mk
@@ -79,7 +79,6 @@ define Device/ampedwireless_b1200ex
   SOC := mt7620a
   DEVICE_VENDOR := Amped Wireless
   DEVICE_MODEL := B1200EX
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
 	edimax-header -s CSYS -m RN10 -f 0x70000 -S 0x01100000 | pad-rootfs | \
@@ -198,7 +197,6 @@ TARGET_DEVICES += comfast_cf-wr800n
 define Device/dlink_dch-m225
   $(Device/seama)
   SOC := mt7620a
-  BLOCKSIZE := 4k
   SEAMA_SIGNATURE := wapn22_dlink.2013gui_dap1320b
   IMAGE_SIZE := 6848k
   DEVICE_VENDOR := D-Link
@@ -384,7 +382,6 @@ define Device/edimax_br-6478ac-v2
   DEVICE_VENDOR := Edimax
   DEVICE_MODEL := BR-6478AC
   DEVICE_VARIANT := V2
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
 	edimax-header -s CSYS -m RN68 -f 0x70000 -S 0x01100000 | pad-rootfs | \
@@ -398,7 +395,6 @@ define Device/edimax_ew-7476rpc
   SOC := mt7620a
   DEVICE_VENDOR := Edimax
   DEVICE_MODEL := EW-7476RPC
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
 	edimax-header -s CSYS -m RN79 -f 0x70000 -S 0x01100000 | pad-rootfs | \
@@ -411,7 +407,6 @@ define Device/edimax_ew-7478ac
   SOC := mt7620a
   DEVICE_VENDOR := Edimax
   DEVICE_MODEL := EW-7478AC
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
 	edimax-header -s CSYS -m RN70 -f 0x70000 -S 0x01100000 | pad-rootfs | \
@@ -424,7 +419,6 @@ define Device/edimax_ew-7478apc
   SOC := mt7620a
   DEVICE_VENDOR := Edimax
   DEVICE_MODEL := EW-7478APC
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
 	edimax-header -s CSYS -m RN75 -f 0x70000 -S 0x01100000 | pad-rootfs | \
@@ -449,7 +443,6 @@ TARGET_DEVICES += elecom_wrh-300cr
 define Device/engenius_epg600
   $(Device/uimage-lzma-loader)
   SOC := mt7620a
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 15616k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := $$(sysupgrade_bin) | check-size | \
@@ -463,7 +456,6 @@ TARGET_DEVICES += engenius_epg600
 
 define Device/engenius_esr600
   SOC := mt7620a
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 15616k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := $$(sysupgrade_bin) | check-size | \
@@ -760,7 +752,6 @@ TARGET_DEVICES += microduino_microwrt
 define Device/netcore_nw5212
   SOC := mt7620a
   IMAGE_SIZE := 16064k
-  BLOCKSIZE := 4k
   DEVICE_VENDOR := Netcore
   DEVICE_MODEL := NW5212
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci
@@ -771,7 +762,6 @@ define Device/netgear_ex2700
   SOC := mt7620a
   NETGEAR_HW_ID := 29764623+4+0+32+2x2+0
   NETGEAR_BOARD_ID := EX2700
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   IMAGES += factory.bin
   KERNEL := $(KERNEL_DTB) | uImage lzma | pad-offset 64k 64 | \
@@ -787,7 +777,6 @@ TARGET_DEVICES += netgear_ex2700
 define Device/netgear_ex3700
   SOC := mt7620a
   NETGEAR_BOARD_ID := U12H319T00_NETGEAR
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGES += factory.chk
   IMAGE/factory.chk := $$(sysupgrade_bin) | check-size | netgear-chk
@@ -801,7 +790,6 @@ TARGET_DEVICES += netgear_ex3700
 define Device/netgear_ex6120
   SOC := mt7620a
   NETGEAR_BOARD_ID := U12H319T30_NETGEAR
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGES += factory.chk
   IMAGE/factory.chk := $$(sysupgrade_bin) | check-size | netgear-chk
@@ -814,7 +802,6 @@ TARGET_DEVICES += netgear_ex6120
 define Device/netgear_ex6130
   SOC := mt7620a
   NETGEAR_BOARD_ID := U12H319T50_NETGEAR
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGES += factory.chk
   IMAGE/factory.chk := $$(sysupgrade_bin) | check-size | netgear-chk
@@ -827,7 +814,6 @@ TARGET_DEVICES += netgear_ex6130
 define Device/netgear_jwnr2010-v5
   $(Device/netgear_sercomm_nor)
   SOC := mt7620n
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3840k
   DEVICE_MODEL := JWNR2010
   DEVICE_VARIANT := v5
@@ -843,7 +829,6 @@ TARGET_DEVICES += netgear_jwnr2010-v5
 define Device/netgear_pr2000
   $(Device/netgear_sercomm_nor)
   SOC := mt7620n
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 15488k
   DEVICE_MODEL := PR2000
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci
@@ -860,7 +845,6 @@ define Device/netgear_wn3000rp-v3
   IMAGE_SIZE := 7872k
   NETGEAR_HW_ID := 29764836+8+0+32+2x2+0
   NETGEAR_BOARD_ID := WN3000RPv3
-  BLOCKSIZE := 4k
   IMAGES += factory.bin
   KERNEL := $(KERNEL_DTB) | uImage lzma | pad-offset 64k 64 | \
 	append-uImage-fakehdr filesystem
@@ -877,7 +861,6 @@ define Device/netgear_wn3100rp-v2
   IMAGE_SIZE := 7872k
   NETGEAR_HW_ID := 29764883+8+0+32+2x2+0
   NETGEAR_BOARD_ID := WN3100RPv2
-  BLOCKSIZE := 4k
   IMAGES += factory.bin
   KERNEL := $(KERNEL_DTB) | uImage lzma | pad-offset 64k 64 | \
 	append-uImage-fakehdr filesystem
@@ -901,7 +884,6 @@ TARGET_DEVICES += netis_wf2770
 
 define Device/nexx_wt3020-4m
   SOC := mt7620n
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | \
@@ -1107,7 +1089,6 @@ TARGET_DEVICES += sercomm_na930
 
 define Device/sitecom_wlr-4100-v1-002
   SOC := mt7620a
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7744k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := $$(sysupgrade_bin) | check-size | \
@@ -1244,7 +1225,6 @@ TARGET_DEVICES += trendnet_tew-810dr
 define Device/vonets_var11n-300
   SOC := mt7620n
   IMAGE_SIZE := 3776k
-  BLOCKSIZE := 4k
   DEVICE_VENDOR := Vonets
   DEVICE_MODEL := VAR11N-300
   DEFAULT := n

target/linux/ramips/image/mt7621.mk
@@ -217,7 +217,6 @@ define Device/arcadyan_we420223-99
   IMAGE_SIZE := 32128k
   KERNEL_SIZE := 4352k
   UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 64k
   PAGESIZE := 1
   SUBPAGESIZE := 1
   VID_HDR_OFFSET := 64
@@ -258,7 +257,6 @@ define Device/asus_rp-ac56
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := RP-AC56
   IMAGE_SIZE := 16000k
-  BLOCKSIZE := 64k
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 \
 	kmod-i2c-ralink kmod-sound-mt7620 -uboot-envtools
   IMAGES += factory.bin
@@ -1409,7 +1407,6 @@ TARGET_DEVICES += jcg_y2
 define Device/keenetic_kn-3010
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 31488k
   DEVICE_VENDOR := Keenetic
   DEVICE_MODEL := KN-3010
@@ -1558,7 +1555,6 @@ TARGET_DEVICES += mediatek_ap-mt7621a-v60
 
 define Device/mediatek_mt7621-eval-board
   $(Device/dsa-migration)
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 15104k
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := MT7621 EVB
@@ -1583,7 +1579,6 @@ TARGET_DEVICES += mercusys_mr70x-v1
 define Device/MikroTik
   $(Device/dsa-migration)
   DEVICE_VENDOR := MikroTik
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 16128k
   DEVICE_PACKAGES := kmod-usb3 -uboot-envtools
   KERNEL_NAME := vmlinuz
@@ -2500,7 +2495,6 @@ TARGET_DEVICES += wavlink_wl-wn533a8
 
 define Device/wavlink_ws-wn572hp3-4g
   $(Device/dsa-migration)
-  BLOCKSIZE := 64k
   DEVICE_VENDOR := Wavlink
   DEVICE_MODEL := WS-WN572HP3
   DEVICE_VARIANT := 4G

target/linux/ramips/image/mt76x8.mk
@@ -50,7 +50,6 @@ endef
 TARGET_DEVICES += asus_rt-ac1200
 
 define Device/asus_rt-ac1200-v2
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := RT-AC1200
@@ -323,7 +322,6 @@ endef
 TARGET_DEVICES += jotale_js76x8-32m
 
 define Device/keenetic_kn-1613
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 31488k
   DEVICE_VENDOR := Keenetic
   DEVICE_MODEL := KN-1613
@@ -381,7 +379,6 @@ endef
 TARGET_DEVICES += mediatek_linkit-smart-7688
 
 define Device/mediatek_mt7628an-eval-board
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := MT7628 EVB
@@ -1040,7 +1037,6 @@ TARGET_DEVICES += zbtlink_zbt-we1226
 
 define Device/zyxel_keenetic-extra-ii
   IMAGE_SIZE := 29824k
-  BLOCKSIZE := 64k
   DEVICE_VENDOR := ZyXEL
   DEVICE_MODEL := Keenetic Extra II
   DEVICE_PACKAGES := kmod-mt76x2 kmod-usb2 kmod-usb-ohci \

target/linux/ramips/image/rt305x.mk
@@ -67,7 +67,6 @@ TARGET_DEVICES += accton_wr6202
 
 define Device/airlive_air3gii
   SOC := rt5350
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := AirLive
   DEVICE_MODEL := Air3GII
@@ -174,7 +173,6 @@ TARGET_DEVICES += argus_atp-52b
 
 define Device/asiarf_awapn2403
   SOC := rt3052
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := AsiaRF
   DEVICE_MODEL := AWAPN2403
@@ -208,7 +206,6 @@ TARGET_DEVICES += asiarf_awm002-evb-8m
 
 define Device/asus_rt-g32-b1
   SOC := rt3050
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := RT-G32
@@ -220,7 +217,6 @@ TARGET_DEVICES += asus_rt-g32-b1
 
 define Device/asus_rt-n10-plus
   SOC := rt3050
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := RT-N10+
@@ -241,7 +237,6 @@ TARGET_DEVICES += asus_rt-n13u
 
 define Device/asus_wl-330n
   SOC := rt3050
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := WL-330N
@@ -252,7 +247,6 @@ TARGET_DEVICES += asus_wl-330n
 
 define Device/asus_wl-330n3g
   SOC := rt3050
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := WL-330N3G
@@ -305,7 +299,6 @@ TARGET_DEVICES += belkin_f7c027
 
 define Device/buffalo_whr-g300n
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3712k
   DEVICE_VENDOR := Buffalo
   DEVICE_MODEL := WHR-G300N
@@ -371,7 +364,6 @@ TARGET_DEVICES += dlink_dir-300-b1
 
 define Device/dlink_dir-300-b7
   SOC := rt5350
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := D-Link
   DEVICE_MODEL := DIR-300
@@ -407,7 +399,6 @@ TARGET_DEVICES += dlink_dir-600-b1
 define Device/dlink_dir-610-a1
   $(Device/seama)
   SOC := rt5350
-  BLOCKSIZE := 4k
   SEAMA_SIGNATURE := wrgn59_dlob.hans_dir610
   KERNEL := $(KERNEL_DTB)
   IMAGE_SIZE := 3776k
@@ -436,7 +427,6 @@ TARGET_DEVICES += dlink_dir-615-d
 define Device/dlink_dir-615-h1
   $(Device/uimage-lzma-loader)
   SOC := rt3352
-  BLOCKSIZE := 4k
   IMAGES += factory.bin
   IMAGE_SIZE := 3776k
   IMAGE/factory.bin := $$(sysupgrade_bin) | senao-header -r 0x218 -p 0x30 -t 3
@@ -525,7 +515,6 @@ TARGET_DEVICES += edimax_3g-6200nl
 
 define Device/engenius_esr-9753
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := EnGenius
   DEVICE_MODEL := ESR-9753
@@ -549,7 +538,6 @@ TARGET_DEVICES += fon_fonera-20n
 
 define Device/hame_mpr-a1
   SOC := rt5350
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   UIMAGE_NAME:= Linux Kernel Image
   DEVICE_VENDOR := HAME
@@ -609,7 +597,6 @@ TARGET_DEVICES += hootoo_ht-tm02
 
 define Device/huawei_d105
   SOC := rt3050
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Huawei
   DEVICE_MODEL := D105
@@ -810,7 +797,6 @@ TARGET_DEVICES += omnima_miniembwifi
 
 define Device/petatel_psr-680w
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Petatel
   DEVICE_MODEL := PSR-680W Wireless 3G Router
@@ -821,7 +807,6 @@ TARGET_DEVICES += petatel_psr-680w
 
 define Device/planex_mzk-dp150n
   SOC := rt5350
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Planex
   DEVICE_MODEL := MZK-DP150N
@@ -943,7 +928,6 @@ TARGET_DEVICES += prolink_pwh2004
 
 define Device/ralink_v22rw-2x2
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Ralink
   DEVICE_MODEL := AP-RT3052-V22RW-2X2
@@ -954,7 +938,6 @@ TARGET_DEVICES += ralink_v22rw-2x2
 
 define Device/sitecom_wl-351
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Sitecom
   DEVICE_MODEL := WL-351 v1
@@ -966,7 +949,6 @@ TARGET_DEVICES += sitecom_wl-351
 
 define Device/skyline_sl-r7205
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Skyline
   DEVICE_MODEL := SL-R7205 Wireless 3G Router
@@ -977,7 +959,6 @@ TARGET_DEVICES += skyline_sl-r7205
 
 define Device/sparklan_wcr-150gn
   SOC := rt3050
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Sparklan
   DEVICE_MODEL := WCR-150GN
@@ -997,7 +978,6 @@ TARGET_DEVICES += teltonika_rut5xx
 
 define Device/tenda_3g150b
   SOC := rt5350
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 3776k
   UIMAGE_NAME:= Linux Kernel Image
   DEVICE_VENDOR := Tenda
@@ -1045,7 +1025,6 @@ TARGET_DEVICES += tenda_w306r-v2
 
 define Device/trendnet_tew-638apb-v2
   SOC := rt3050
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   IMAGE/sysupgrade.bin := $$(sysupgrade_bin) | umedia-header 0x026382 | \
 	check-size | append-metadata
@@ -1103,7 +1082,6 @@ TARGET_DEVICES += unbranded_wr512-3gn-8m
 
 define Device/unbranded_xdx-rn502j
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := Unbranded
   DEVICE_MODEL := XDX-RN502J
@@ -1114,7 +1092,6 @@ TARGET_DEVICES += unbranded_xdx-rn502j
 
 define Device/upvel_ur-326n4g
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := UPVEL
   DEVICE_MODEL := UR-326N4G
@@ -1197,7 +1174,6 @@ TARGET_DEVICES += zte_mf283plus
 
 define Device/zyxel_keenetic
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := ZyXEL
   DEVICE_MODEL := Keenetic
@@ -1238,7 +1214,6 @@ TARGET_DEVICES += zyxel_keenetic-start
 
 define Device/zyxel_nbg-419n
   SOC := rt3052
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 3776k
   DEVICE_VENDOR := ZyXEL
   DEVICE_MODEL := NBG-419N

target/linux/ramips/image/rt3883.mk
@@ -7,7 +7,6 @@ endef
 
 define Device/asus_rt-n56u
   SOC := rt3662
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   IMAGE/sysupgrade.bin += | mkrtn56uimg -s
   DEVICE_VENDOR := ASUS
@@ -20,7 +19,6 @@ TARGET_DEVICES += asus_rt-n56u
 define Device/belkin_f9k1109v1
   $(Device/uimage-lzma-loader)
   SOC := rt3883
-  BLOCKSIZE := 64k
   DEVICE_VENDOR := Belkin
   DEVICE_MODEL := F9K1109
   DEVICE_VARIANT := Version 1.0
@@ -34,7 +32,6 @@ TARGET_DEVICES += belkin_f9k1109v1
 define Device/dlink_dir-645
   $(Device/seama-lzma-loader)
   SOC := rt3662
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7872k
   SEAMA_SIGNATURE := wrgn39_dlob.hans_dir645
   DEVICE_VENDOR := D-Link
@@ -46,7 +43,6 @@ TARGET_DEVICES += dlink_dir-645
 
 define Device/edimax_br-6475nd
   SOC := rt3662
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
 	edimax-header -s CSYS -m RN54 -f 0x70000 -S 0x01100000 | pad-rootfs | \
@@ -60,7 +56,6 @@ TARGET_DEVICES += edimax_br-6475nd
 define Device/engenius_esr600h
   $(Device/uimage-lzma-loader)
   SOC := rt3662
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7872k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := $$(sysupgrade_bin) | check-size | \
@@ -73,7 +68,6 @@ TARGET_DEVICES += engenius_esr600h
 
 define Device/loewe_wmdr-143n
   SOC := rt3662
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := Loewe
   DEVICE_MODEL := WMDR-143N
@@ -83,7 +77,6 @@ TARGET_DEVICES += loewe_wmdr-143n
 
 define Device/omnima_hpm
   SOC := rt3662
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := Omnima
   DEVICE_MODEL := HPM
@@ -108,7 +101,6 @@ TARGET_DEVICES += samsung_cy-swr1100
 define Device/sitecom_wlr-6000
   $(Device/uimage-lzma-loader)
   SOC := rt3883
-  BLOCKSIZE := 4k
   IMAGE_SIZE := 7244k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := $$(sysupgrade_bin) | check-size | \
@@ -122,7 +114,6 @@ TARGET_DEVICES += sitecom_wlr-6000
 
 define Device/trendnet_tew-691gr
   SOC := rt3883
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | umedia-header 0x026910
@@ -134,7 +125,6 @@ TARGET_DEVICES += trendnet_tew-691gr
 
 define Device/trendnet_tew-692gr
   SOC := rt3883
-  BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | umedia-header 0x026920

commit 15b231f42ea2fe6e49512f8ab48600f1e6a51e7c

ramips: fix build error on Airlink AR670W
The 'KERNEL' is not referenced by other objects, so double '$$' will
cause shell unable to parse the variable 'BLOCKSIZE':
  dd ... bs=$(BLOCKSIZE) conv=sync
  bash: line 1: BLOCKSIZE: command not found

Fixes: dbb95c2cc7(ramips: set default BLOCKSIZE to 64k for nor flash devices)
Signed-off-by: Shiji Yang <yangshiji66@qq.com>

target/linux/ramips/image/rt288x.mk
@@ -15,7 +15,7 @@ define Device/airlink101_ar670w
   DEVICE_VENDOR := Airlink
   DEVICE_MODEL := AR670W
   IMAGE_SIZE := 3840k
-  KERNEL := $(KERNEL_DTB) | pad-to $$$$(BLOCKSIZE)
+  KERNEL := $(KERNEL_DTB) | pad-to $$(BLOCKSIZE)
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | \
 	wrg-header wrgn16a_airlink_ar670w