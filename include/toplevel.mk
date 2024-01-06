LuisMitaHL/LooperWrt

commit 9e41c66a57e6d419138da9b04cd11cb5384ec25c

build: allow var.% targets to bypass the prepare steps
These targets are used to input variable values from the Make
context into other things like python scripts, so log messages
should be silenced and build prerequisites should be skipped.

The same thing is done for the other variable print target "val.%".

While at it, combine identical target rules into one definition.

Signed-off-by: Michael Pratt <mcpratt@pm.me>

include/toplevel.mk
@@ -210,9 +210,7 @@ prereq:: prepare-tmpinfo .config
 	@+$(NO_TRACE_MAKE) -r -s $@
 
 check: .config FORCE
-	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
-
-val.%: FORCE
+check val.% var.%: FORCE
 	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
 
 WARN_PARALLEL_ERROR = $(if $(BUILD_LOG),,$(and $(filter -j,$(MAKEFLAGS)),$(findstring s,$(OPENWRT_VERBOSE))))

commit c783b41aa5a41b1b7a51163fc520e72cbf41dcf6

build: fix toplevel check target
Partially revert changes to verbose logging that break the 'check' target
dependencies and trigger many runtime warnings like:
  /home/kodidev/openwrt-project/include/toplevel.mk:213: *** mixed implicit and normal rules: deprecated syntax

Fixes: 9e41c66a5 ("build: allow var.% targets to bypass the prepare steps")
Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

include/toplevel.mk
@@ -210,7 +210,9 @@ prereq:: prepare-tmpinfo .config
 	@+$(NO_TRACE_MAKE) -r -s $@
 
 check: .config FORCE
-check val.% var.%: FORCE
+	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
+
+val.% var.%: FORCE
 	@+$(NO_TRACE_MAKE) -r -s $@ QUIET= V=s
 
 WARN_PARALLEL_ERROR = $(if $(BUILD_LOG),,$(and $(filter -j,$(MAKEFLAGS)),$(findstring s,$(OPENWRT_VERBOSE))))