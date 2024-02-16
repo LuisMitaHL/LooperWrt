LuisMitaHL/LooperWrt

commit 4f840393a80c45f9fb8f8d63c2317f8560d6b1fd

scripts: ubinize-image.sh: support static volumes, make size optional
In order to support devices having TF-A FIP image or UBI-aware U-Boot
SPL we need to include a static volume for the bootloader.

Introduce support for adding additional static volumes by prefixing
the filename with ':', eg.

UBINIZE_PARTS := fip:=$(STAGING_DIR_IMAGE)/u-boot.fip

Also add support for rootfs-in-uImage.FIT setups which don't require a
rootfs partition and make the (3rd) size parameter in UBINIZE_PARTS
optional (see example above without declared size).

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

include/image-commands.mk
@@ -138,9 +138,11 @@ UBI_NAND_SIZE_LIMIT = $(IMAGE_SIZE) - ($(NAND_SIZE)*20/1024 + 4*$(BLOCKSIZE))
 define Build/append-ubi
 	sh $(TOPDIR)/scripts/ubinize-image.sh \
 		$(if $(UBOOTENV_IN_UBI),--uboot-env) \
-		$(if $(KERNEL_IN_UBI),--kernel $(IMAGE_KERNEL)) \
 		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
-		--rootfs $(IMAGE_ROOTFS) \
+		$(if $(findstring fit,$(1)), \
+		$(if $(KERNEL_IN_UBI),--part fit=$(IMAGE_KERNEL)), \
+		$(if $(KERNEL_IN_UBI),--kernel $(IMAGE_KERNEL)) \
+		--rootfs $(IMAGE_ROOTFS)) \
 		$@.tmp \
 		-p $(BLOCKSIZE:%k=%KiB) -m $(PAGESIZE) \
 		$(if $(SUBPAGESIZE),-s $(SUBPAGESIZE)) \

scripts/ubinize-image.sh
@@ -12,15 +12,16 @@ err=""
 ubinize_seq=""
 
 ubivol() {
-	volid=$1
-	name=$2
-	image=$3
-	autoresize=$4
-	size="$5"
+	local volid=$1
+	local name=$2
+	local image=$3
+	local autoresize=$4
+	local size="$5"
+	local voltype="${6:-dynamic}"
 	echo "[$name]"
 	echo "mode=ubi"
 	echo "vol_id=$volid"
-	echo "vol_type=dynamic"
+	echo "vol_type=$voltype"
 	echo "vol_name=$name"
 	if [ "$image" ]; then
 		echo "image=$image"
@@ -38,6 +39,7 @@ ubilayout() {
 	local rootsize=
 	local autoresize=
 	local rootfs_type="$( get_fs_type "$2" )"
+	local voltype
 
 	if [ "$1" = "ubootenv" ]; then
 		ubivol $vol_id ubootenv
@@ -49,16 +51,26 @@ ubilayout() {
 		name="${part%%=*}"
 		prev="$part"
 		part="${part#*=}"
+		voltype=dynamic
 		[ "$prev" = "$part" ] && part=
 
 		image="${part%%=*}"
+		if [ "${image:0:1}" = ":" ]; then
+			voltype=static
+			image="${image:1}"
+		fi
 		prev="$part"
 		part="${part#*=}"
 		[ "$prev" = "$part" ] && part=
 
 		size="$part"
+		if [ -z "$size" ]; then
+			size="$( round_up "$( stat -c%s "$image" )" 1024 )"
+		else
+			size="${size}MiB"
+		fi
 
-		ubivol $vol_id "$name" "$image" "" "${size}MiB"
+		ubivol $vol_id "$name" "$image" "" "${size}" "$voltype"
 		vol_id=$(( $vol_id + 1 ))
 	done
 	if [ "$3" ]; then
@@ -77,10 +89,10 @@ ubilayout() {
 			rootsize="$( round_up "$( stat -c%s "$2" )" 1024 )"
 			;;
 		esac
-		ubivol $vol_id rootfs "$2" "$autoresize" "$rootsize"
+		ubivol $vol_id rootfs "$2" "$autoresize" "$rootsize" dynamic
 
 		vol_id=$(( $vol_id + 1 ))
-		[ "$rootfs_type" = "ubifs" ] || ubivol $vol_id rootfs_data "" 1
+		[ "$rootfs_type" = "ubifs" ] || ubivol $vol_id rootfs_data "" 1 dymamic
 	fi
 }
 

commit e352c842d70a08842edc7294fa10baafa7aa1900

scripts: ubinize-image.sh: fix on POSIX shell, allow custom images
Make sure ubinize-image.sh also works with more simple POSIX Shell and
allow creating complete custom images to be used as ARTIFACT/foo.img
and thereby allow including uImage.FIT, TF-A FIP and what ever else
is required on a specific board.

Fixes: 4f840393a8 ("scripts: ubinize-image.sh: support static volumes, make size optional")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

include/image-commands.mk
@@ -138,11 +138,9 @@ UBI_NAND_SIZE_LIMIT = $(IMAGE_SIZE) - ($(NAND_SIZE)*20/1024 + 4*$(BLOCKSIZE))
 define Build/append-ubi
 	sh $(TOPDIR)/scripts/ubinize-image.sh \
 		$(if $(UBOOTENV_IN_UBI),--uboot-env) \
-		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
-		$(if $(findstring fit,$(1)), \
-		$(if $(KERNEL_IN_UBI),--part fit=$(IMAGE_KERNEL)), \
 		$(if $(KERNEL_IN_UBI),--kernel $(IMAGE_KERNEL)) \
-		--rootfs $(IMAGE_ROOTFS)) \
+		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
+		--rootfs $(IMAGE_ROOTFS) \
 		$@.tmp \
 		-p $(BLOCKSIZE:%k=%KiB) -m $(PAGESIZE) \
 		$(if $(SUBPAGESIZE),-s $(SUBPAGESIZE)) \
@@ -152,6 +150,18 @@ define Build/append-ubi
 	rm $@.tmp
 	$(if $(and $(IMAGE_SIZE),$(NAND_SIZE)),\
 		$(call Build/check-size,$(UBI_NAND_SIZE_LIMIT)))
+endef
+
+define Build/ubinize-image
+	sh $(TOPDIR)/scripts/ubinize-image.sh \
+		$(if $(UBOOTENV_IN_UBI),--uboot-env) \
+		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
+		--part $(word 1,$(1))="$(BIN_DIR)/$(DEVICE_IMG_PREFIX)-$(word 2,$(1))" \
+		$@ \
+		-p $(BLOCKSIZE:%k=%KiB) -m $(PAGESIZE) \
+		$(if $(SUBPAGESIZE),-s $(SUBPAGESIZE)) \
+		$(if $(VID_HDR_OFFSET),-O $(VID_HDR_OFFSET)) \
+		$(UBINIZE_OPTS)
 endef
 
 define Build/ubinize-kernel

scripts/ubinize-image.sh
@@ -12,10 +12,10 @@ err=""
 ubinize_seq=""
 
 ubivol() {
-	local volid=$1
-	local name=$2
-	local image=$3
-	local autoresize=$4
+	local volid="$1"
+	local name="$2"
+	local image="$3"
+	local autoresize="$4"
 	local size="$5"
 	local voltype="${6:-dynamic}"
 	echo "[$name]"
@@ -36,16 +36,17 @@ ubivol() {
 
 ubilayout() {
 	local vol_id=0
-	local rootsize=
-	local autoresize=
-	local rootfs_type="$( get_fs_type "$2" )"
+	local rootsize
+	local autoresize
+	local rootfs_type
 	local voltype
 
+	rootfs_type="$( get_fs_type "$2" )"
 	if [ "$1" = "ubootenv" ]; then
 		ubivol $vol_id ubootenv
-		vol_id=$(( $vol_id + 1 ))
+		vol_id=$(( vol_id + 1 ))
 		ubivol $vol_id ubootenv2
-		vol_id=$(( $vol_id + 1 ))
+		vol_id=$(( vol_id + 1 ))
 	fi
 	for part in $parts; do
 		name="${part%%=*}"
@@ -55,9 +56,9 @@ ubilayout() {
 		[ "$prev" = "$part" ] && part=
 
 		image="${part%%=*}"
-		if [ "${image:0:1}" = ":" ]; then
+		if [ "${image#:}" != "$image" ]; then
 			voltype=static
-			image="${image:1}"
+			image="${image#:}"
 		fi
 		prev="$part"
 		part="${part#*=}"
@@ -71,11 +72,11 @@ ubilayout() {
 		fi
 
 		ubivol $vol_id "$name" "$image" "" "${size}" "$voltype"
-		vol_id=$(( $vol_id + 1 ))
+		vol_id=$(( vol_id + 1 ))
 	done
 	if [ "$3" ]; then
 		ubivol $vol_id kernel "$3"
-		vol_id=$(( $vol_id + 1 ))
+		vol_id=$(( vol_id + 1 ))
 	fi
 
 	if [ "$2" ]; then
@@ -91,7 +92,7 @@ ubilayout() {
 		esac
 		ubivol $vol_id rootfs "$2" "$autoresize" "$rootsize" dynamic
 
-		vol_id=$(( $vol_id + 1 ))
+		vol_id=$(( vol_id + 1 ))
 		[ "$rootfs_type" = "ubifs" ] || ubivol $vol_id rootfs_data "" 1 dymamic
 	fi
 }
@@ -128,7 +129,7 @@ while [ "$1" ]; do
 		continue
 		;;
 	"-"*)
-		ubinize_param="$@"
+		ubinize_param="$*"
 		break
 		;;
 	*)
@@ -141,7 +142,7 @@ while [ "$1" ]; do
 	esac
 done
 
-if [ ! -r "$rootfs" -a ! -r "$kernel" -a ! "$outfile" ]; then
+if [ ! -r "$rootfs" ] && [ ! -r "$kernel" ] && [ ! "$parts" ] && [ ! "$outfile" ]; then
 	echo "syntax: $0 [--uboot-env] [--part <name>=<file>] [--kernel kernelimage] [--rootfs rootfsimage] out [ubinize opts]"
 	exit 1
 fi