LuisMitaHL/LooperWrt

commit 6e745a80411652313572bd029c944fba9f141f1d

include/package-pack: set missing Description on .apk creation
Set missing description info on .apk creation. This was probably a TODO
that wasn't notice when the final implementation was pushed.

Fixes: 5076f34d4a19 ("build: add APK package build capabilities")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/package-pack.mk
@@ -339,7 +339,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  --info "description:" \
+	  --info "description: $$(strip $$(Package/$(1)/description))" \
 	  --info "arch:$(PKGARCH)" \
 	  --info "license:$(LICENSE)" \
 	  --info "origin:$(SOURCE)" \

commit a0f46a41f338f95194dc4ec2a8af5ce9b0a0b6ea

include/package-pack: remove whitespace for Description info for APK
Remove whitespace for Description info for .apk creation

Fixes: 6e745a804116 ("include/package-pack: set missing Description on .apk creation")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/package-pack.mk
@@ -339,7 +339,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  --info "description: $$(strip $$(Package/$(1)/description))" \
+	  --info "description:$$(strip $$(Package/$(1)/description))" \
 	  --info "arch:$(PKGARCH)" \
 	  --info "license:$(LICENSE)" \
 	  --info "origin:$(SOURCE)" \