LuisMitaHL/LooperWrt

commit c5fcdd2b1da282b236f42967f386e71fc6a67728

mediatek: add support for BananaPi BPi-R4 Lite
The BPi-R4 Lite is a WiFi-7 router board based on the MT7987 SoC.

Specification :
 - SOC: Mediatek MT7987A (4x Cortex-A53
 - RAM: 2GB
 - Flash: 32MB SPI NOR, 256MB SPI NAND, 8GB eMMC
 - Switch: MediaTek MT7531AE
 - Ports : 4x LAN (1G), 1x SFP (via MT7531), 1x WAN (2.5G)
 - Buttons : Reset & WPS/Mesh
 - LEDs : Status (PWM), SFP
 - USB: on-board VIA VL817 USB3.1/USB2.0 hub
    * 1 - mPCIe B (SIM3)
    * 2 - NGFF-KEYB (SIM1)
    * 3 - USB-A connector
    * 4 - mPCIe A (SIM4)
 - mPCIe: 1x 8GT/s x2 or 2x 8GT/s x1 (configurable via bootloader)
 - RTC: PCF8563
 - mikroBUS socket with SPI, I2C and full UART
 - on-board HT42B534 USB-to-serial for Type-C console port
 - Power: USB Type-C PD 20V, or DC via barrel connector or JST-VH 3.96

Installation:
Uncompress *sdcard.img.gz and write to microSD card, eg. using 'dd'.
Use bootloader menu on the serial console to install SPI-NAND or SPI-NOR,
once installed to SPI-NAND you can use the bootloader menu to install to
eMMC. See instructions for BananaPi R3 for details.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/filogic.mk
@@ -638,6 +638,67 @@ endif
 endef
 TARGET_DEVICES += bananapi_bpi-r4-poe
 
+define Device/bananapi_bpi-r4-lite
+  DEVICE_VENDOR := Bananapi
+  DEVICE_MODEL := BPi-R4 Lite
+  DEVICE_DTS := mt7987a-bananapi-bpi-r4-lite
+  DEVICE_DTS_OVERLAY:= mt7987a-bananapi-bpi-r4-lite-1pcie-2L mt7987a-bananapi-bpi-r4-lite-2pcie-1L \
+		       mt7987a-bananapi-bpi-r4-lite-emmc mt7987a-bananapi-bpi-r4-lite-sd \
+		       mt7987a-bananapi-bpi-r4-lite-nand mt7987a-bananapi-bpi-r4-lite-nor
+  DEVICE_DTS_CONFIG := config-mt7987a-bananapi-bpi-r4-lite
+  DEVICE_DTC_FLAGS := --pad 4096
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTS_LOADADDR := 0x4ff00000
+  DEVICE_PACKAGES := mt7987-2p5g-phy-firmware-internal kmod-eeprom-at24 \
+		     kmod-gpio-pca953x kmod-i2c-mux-pca954x kmod-rtc-pcf8563 \
+		     kmod-sfp e2fsprogs mkf2fs
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  UBOOTENV_IN_UBI := 1
+  KERNEL_LOADADDR := 0x40000000
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+        fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  IMAGE/sysupgrade.itb := append-kernel | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
+  ARTIFACTS := \
+	       emmc-preloader.bin emmc-bl31-uboot.fip \
+	       nor-preloader.bin nor-bl31-uboot.fip \
+	       sdcard.img.gz \
+	       snand-preloader.bin snand-bl31-uboot.fip
+  ARTIFACT/emmc-preloader.bin	:= mt7987-bl2 emmc-comb
+  ARTIFACT/emmc-bl31-uboot.fip	:= mt7987-bl31-uboot bananapi_bpi-r4-lite-emmc
+  ARTIFACT/nor-preloader.bin	:= mt7987-bl2 nor-comb
+  ARTIFACT/nor-bl31-uboot.fip	:= mt7987-bl31-uboot bananapi_bpi-r4-lite-nor
+  ARTIFACT/snand-preloader.bin	:= mt7987-bl2 spim-nand2-ubi-comb
+  ARTIFACT/snand-bl31-uboot.fip	:= mt7987-bl31-uboot bananapi_bpi-r4-lite-snand
+  ARTIFACT/sdcard.img.gz	:= mt798x-gpt sdmmc |\
+				   pad-to 17k | mt7987-bl2 sdmmc-comb |\
+				   pad-to 6656k | mt7987-bl31-uboot bananapi_bpi-r4-lite-sdmmc |\
+				$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),\
+				   pad-to 12M | append-image-stage initramfs-recovery.itb | check-size 44m |\
+				) \
+				   pad-to 44M | mt7987-bl2 spim-nand2-ubi-comb |\
+				   pad-to 45M | mt7987-bl31-uboot bananapi_bpi-r4-lite-snand |\
+				   pad-to 49M | mt7987-bl2 nor-comb |\
+				   pad-to 50M | mt7987-bl31-uboot bananapi_bpi-r4-lite-nor |\
+				   pad-to 51M | mt7987-bl2 emmc-comb |\
+				   pad-to 52M | mt7987-bl31-uboot bananapi_bpi-r4-lite-emmc |\
+				   pad-to 56M | mt798x-gpt emmc |\
+				$(if $(CONFIG_TARGET_ROOTFS_SQUASHFS),\
+				   pad-to 64M | append-image squashfs-sysupgrade.itb | check-size |\
+				) \
+				  gzip
+ifeq ($(DUMP),)
+  IMAGE_SIZE := $$(shell expr 64 + $$(CONFIG_TARGET_ROOTFS_PARTSIZE))m
+endif
+endef
+TARGET_DEVICES += bananapi_bpi-r4-lite
+
 define Device/buffalo_wsr-6000ax8
   DEVICE_MODEL := WSR-6000AX8
   DEVICE_VENDOR := Buffalo

target/linux/mediatek/base-files/etc/uci-defaults/99_fwenv-store-ethaddr.sh
@@ -13,6 +13,7 @@ bananapi,bpi-r3|\
 bananapi,bpi-r3-mini|\
 bananapi,bpi-r4|\
 bananapi,bpi-r4-2g5|\
+bananapi,bpi-r4-lite|\
 bananapi,bpi-r4-poe)
 	[ -z "$(fw_printenv -n ethaddr 2>/dev/null)" ] &&
 		fw_setenv ethaddr "$(cat /sys/class/net/eth0/address)"

target/linux/mediatek/dts/mt7987a-bananapi-bpi-r4-lite-1pcie-2L.dtso
@@ -1,0 +1,35 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+/*
+ * Copyright (C) 2025 MediaTek Inc.
+ * Author: Sam.Shih <sam.shih@mediatek.com>
+ */
+
+/dts-v1/;
+/plugin/;
+
+#include <dt-bindings/gpio/gpio.h>
+
+/ {
+	compatible = "bananapi,bpi-r4-lite",
+		     "mediatek,mt7987a", "mediatek,mt7987";
+};
+
+/ {
+	fragment@0 {
+		target-path = "/soc/pinctrl@1001f000";
+		__overlay__ {
+			pcie_sel: aw35710qnr_sel {
+				gpio-hog;
+				gpios = <11 GPIO_ACTIVE_HIGH>;
+				output-high;
+			};
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc/pcie@11290000";
+		__overlay__ {
+			status = "disabled";
+		};
+	};
+};

target/linux/mediatek/dts/mt7987a-bananapi-bpi-r4-lite-2pcie-1L.dtso
@@ -1,0 +1,36 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+/*
+ * Copyright (C) 2025 MediaTek Inc.
+ * Author: Sam.Shih <sam.shih@mediatek.com>
+ */
+
+/dts-v1/;
+/plugin/;
+
+#include <dt-bindings/gpio/gpio.h>
+
+/ {
+	compatible = "bananapi,bpi-r4-lite",
+		     "mediatek,mt7987a", "mediatek,mt7987";
+};
+
+/ {
+	fragment@0 {
+		target-path = "/soc/pinctrl@1001f000";
+		__overlay__ {
+			pcie_sel: aw35710qnr_sel {
+				gpio-hog;
+				gpios = <11 GPIO_ACTIVE_HIGH>;
+				output-low;
+			};
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc/pcie@11290000";
+		__overlay__ {
+			status = "okay";
+		};
+	};
+};
+

target/linux/mediatek/dts/mt7987a-bananapi-bpi-r4-lite-emmc.dtso
@@ -1,0 +1,54 @@
+/dts-v1/;
+/plugin/;
+
+#include <dt-bindings/gpio/gpio.h>
+
+/ {
+	compatible = "bananapi,bpi-r4-lite",
+		     "mediatek,mt7987a", "mediatek,mt7987";
+};
+
+&{/soc/mmc@11230000} {
+	pinctrl-names = "default", "state_uhs";
+	pinctrl-0 = <&mmc_pins_default>;
+	pinctrl-1 = <&mmc_pins_uhs>;
+	reset-gpios = <&pio 14 GPIO_ACTIVE_LOW>;
+
+	bus-width = <4>; /* interference from shared DAT5<->SD-CD pin */
+	max-frequency = <48000000>;
+	cap-mmc-highspeed;
+	non-removable;
+	no-sd;
+	no-sdio;
+	vmmc-supply = <&reg_3p3v>;
+	vqmmc-supply = <&reg_3p3v>;
+
+	#address-cells = <1>;
+	#size-cells = <0>;
+
+	status = "okay";
+
+	card@0 {
+		compatible = "mmc-card";
+		reg = <0>;
+
+		partitions {
+			compatible = "gpt-partitions";
+
+			block-partition-env {
+				partname = "ubootenv";
+				nvmem-layout {
+					compatible = "u-boot,env-layout";
+				};
+			};
+
+			emmc_rootfs: block-partition-production {
+				partname = "production";
+			};
+		};
+	};
+};
+
+&{/chosen} {
+	rootdisk-emmc = <&emmc_rootfs>;
+};

target/linux/mediatek/dts/mt7987a-bananapi-bpi-r4-lite-mikrobus.dtsi
@@ -1,0 +1,134 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+/*
+ * Copyright (C) 2025 MediaTek Inc.
+ * Author: Sam.Shih <sam.shih@mediatek.com>
+ */
+
+#include <dt-bindings/gpio/gpio.h>
+
+/*
+ * --------------- Bananapi Bpi R4 Lite MikroBus ----------------
+ * | 9  |                           GPIO7/PWM1              | 8 |
+ * | 10 | GPIO6/PCM_FS/UART1_RTS    GPIO5/PCM_CLK/UART1_CTS | 7 |
+ * | 11 | GPIO49/SPI1_CS            GPIO3/PCM_DTX/UART1_RXD | 6 |
+ * | 12 | GPIO46/SPI1_CLK           GPIO4/PCM_DRX/UART1_TXD | 5 |
+ * | 13 | GPIO48/SPI1_MISO          I2C3 SCL (PCA9548)      | 4 |
+ * | 14 | GPIO47/SPI1_MOSI          I2C3 SDA (PCA9548)      | 3 |
+ * | 15 | +3.3V                     +5V                     | 2 |
+ * | 16 | GND                       GND                     | 1 |
+ * --------------------------------------------------------------
+ */
+
+/ {
+	mikrobus: MikroBus {
+		compatible = "simple-bus";
+
+		/* MikroBus pins 5,6,7,8 as GPIOs */
+		mikrobus_gpios_0: gpio-export@0 {
+			compatible = "gpio-export";
+			status = "disabled";
+
+			gpio-3 {
+				gpio-export,name = "mikrobus-gpio-RX";
+				gpio-export,output = <1>;
+				gpios = <&pio 3 GPIO_ACTIVE_HIGH>;
+			};
+
+			gpio-4 {
+				gpio-export,name = "mikrobus-gpio-TX";
+				gpio-export,output = <1>;
+				gpios = <&pio 4 GPIO_ACTIVE_HIGH>;
+			};
+
+			gpio-5 {
+				gpio-export,name = "mikrobus-gpio-INT";
+				gpio-export,output = <1>;
+				gpios = <&pio 5 GPIO_ACTIVE_HIGH>;
+			};
+
+			gpio-6 {
+				gpio-export,name = "mikrobus-gpio-INT";
+				gpio-export,output = <1>;
+				gpios = <&pio 5 GPIO_ACTIVE_HIGH>;
+			};
+		};
+
+		/* MikroBus pins 11,12,13,14 as GPIOs */
+		mikrobus_gpios_1: gpio-export@1 {
+			compatible = "gpio-export";
+			status = "disabled";
+
+			gpio-46 {
+				gpio-export,name = "mikrobus-gpio-CS";
+				gpio-export,output = <1>;
+				gpios = <&pio 46 GPIO_ACTIVE_HIGH>;
+			};
+
+			gpio-47 {
+				gpio-export,name = "mikrobus-gpio-MOSI";
+				gpio-export,output = <1>;
+				gpios = <&pio 47 GPIO_ACTIVE_HIGH>;
+			};
+
+			gpio-48 {
+				gpio-export,name = "mikrobus-gpio-MISO";
+				gpio-export,output = <1>;
+				gpios = <&pio 48 GPIO_ACTIVE_HIGH>;
+			};
+
+			gpio-49 {
+				gpio-export,name = "mikrobus-gpio-SCK";
+				gpio-export,output = <1>;
+				gpios = <&pio 49 GPIO_ACTIVE_HIGH>;
+			};
+		};
+	};
+};
+
+/* MikroBus pins 3,4 as I2C3 (Provided by PCA9548 i2c-mux) */
+/* Please refer to &imux2_MikroBus device tree node */
+
+/* MikroBus pins 5,6,7,10 as PCM function */
+&afe {
+	pinctrl-names = "default";
+	pinctrl-0 = <&pcm_pins>;
+	status = "disabled";
+};
+
+&pcm_pins {
+	mux {
+		function = "pcm";
+		groups = "pcm0_0";
+	};
+};
+
+/* MikroBus pins 5,6,7,10 as UART1 function */
+&uart1 {
+	pinctrl-names = "default";
+	pinctrl-0 = <&uart1_pins>;
+	status = "okay";
+};
+
+&uart1_pins {
+	mux {
+		function = "uart";
+		groups = "uart1_0";
+	};
+};
+
+/* MikroBus pins 8 as PWM1 */
+/* Please refer to &pwm device tree node */
+
+/* MikroBus pins 11,12,13,14 as SPI1 function */
+&spi1 {
+	pinctrl-names = "default";
+	pinctrl-0 = <&spic_pins>;
+	status = "okay";
+};
+
+&spic_pins {
+	mux {
+		function = "spi";
+		groups = "spi1_1";
+	};
+};

target/linux/mediatek/dts/mt7987a-bananapi-bpi-r4-lite-nand.dtso
@@ -1,0 +1,61 @@
+/dts-v1/;
+/plugin/;
+
+/ {
+	compatible = "bananapi,bpi-r4-lite",
+		     "mediatek,mt7987a", "mediatek,mt7987";
+};
+
+&{/soc/spi@11009800} {
+	#address-cells = <1>;
+	#size-cells = <0>;
+
+	flash@0 {
+		compatible = "spi-nand";
+		reg = <0>;
+		spi-max-frequency = <52000000>;
+		spi-rx-bus-width = <4>;
+		spi-tx-bus-width = <4>;
+
+		partitions {
+			compatible = "fixed-partitions";
+			#address-cells = <1>;
+			#size-cells = <1>;
+
+			partition@0 {
+				label = "bl2-nand";
+				reg = <0x0 0x200000>;
+			};
+
+			partition@200000 {
+				label = "ubi";
+				reg = <0x200000 0xfe00000>;
+				compatible = "linux,ubi";
+
+				volumes {
+					ubi-volume-ubootenv {
+						volname = "ubootenv";
+						nvmem-layout {
+							compatible = "u-boot,env-redundant-bool-layout";
+						};
+					};
+
+					ubi-volume-ubootenv2 {
+						volname = "ubootenv2";
+						nvmem-layout {
+							compatible = "u-boot,env-redundant-bool-layout";
+						};
+					};
+
+					ubi_rootfs: ubi-volume-fit {
+						volname = "fit";
+					};
+				};
+			};
+		};
+	};
+};
+
+&{/chosen} {
+	rootdisk-spim-nand = <&ubi_rootfs>;
+};

target/linux/mediatek/dts/mt7987a-bananapi-bpi-r4-lite-nor.dtso
@@ -1,0 +1,63 @@
+/dts-v1/;
+/plugin/;
+
+/ {
+	compatible = "bananapi,bpi-r4-lite",
+		     "mediatek,mt7987a", "mediatek,mt7987";
+};
+
+&{/soc/spi@11009800} {
+	#address-cells = <1>;
+	#size-cells = <0>;
+
+	flash@0 {
+		compatible = "jedec,spi-nor";
+		reg = <0>;
+		spi-max-frequency = <52000000>;
+		spi-rx-bus-width = <4>;
+		spi-tx-bus-width = <4>;
+
+		partitions {
+			compatible = "fixed-partitions";
+			#address-cells = <1>;
+			#size-cells = <1>;
+
+			partition@0 {
+				label = "bl2-nor";
+				reg = <0x0 0x40000>;
+				read-only;
+			};
+
+			partition@40000 {
+				label = "u-boot-env";
+				reg = <0x40000 0x40000>;
+			};
+
+			partition@50000 {
+				label = "factory";
+				reg = <0x80000 0x80000>;
+				read-only;
+			};
+
+			partition@100000 {
+				label = "fip";
+				reg = <0x100000 0x80000>;
+			};
+
+			partition@180000 {
+				label = "recovery";
+				reg = <0x180000 0xe80000>;
+			};
+
+			nor_rootfs: partition@1000000 {
+				label = "fit";
+				reg = <0x1000000 0x1000000>;
+				compatible = "denx,fit";
+			};
+		};
+	};
+};
+
+&{/chosen} {
+	rootdisk-nor = <&nor_rootfs>;
+};

commit 6670465d4cb0abf4cb73b4bffbcda91cf4645c10

mediatek: remove non-existing mt7987-2p5g-phy-firmware-internal
Because the firmware has not yet been accepted in linux-firmware we
cannot yet package mt7987-2p5g-phy-firmware-internal. Remove it from
DEVICE_PACKAGES of the BPi-R4-mini until the an upcoming linux-firmware
release will come with this firmware included, allowing us to then
create that package.

Fixes: c5fcdd2b1d ("mediatek: add support for BananaPi BPi-R4 Lite")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/filogic.mk
@@ -649,7 +649,7 @@ define Device/bananapi_bpi-r4-lite
   DEVICE_DTC_FLAGS := --pad 4096
   DEVICE_DTS_DIR := ../dts
   DEVICE_DTS_LOADADDR := 0x4ff00000
-  DEVICE_PACKAGES := mt7987-2p5g-phy-firmware-internal kmod-eeprom-at24 \
+  DEVICE_PACKAGES := kmod-eeprom-at24 \
 		     kmod-gpio-pca953x kmod-i2c-mux-pca954x kmod-rtc-pcf8563 \
 		     kmod-sfp e2fsprogs mkf2fs
   BLOCKSIZE := 128k